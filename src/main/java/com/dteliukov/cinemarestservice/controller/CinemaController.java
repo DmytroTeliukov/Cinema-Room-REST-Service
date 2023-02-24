09dda87a603b02a56bb15e0931b24346747d7200
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-02-24 16:31:57 +0200

    Add endpoint "/stats" in controller CinemaController

--- src/main/java/com/dteliukov/cinemarestservice/controller/CinemaController.java
+++ src/main/java/com/dteliukov/cinemarestservice/controller/CinemaController.java
@@ -3,11 +3,13 @@ package com.dteliukov.cinemarestservice.controller;
 import com.dteliukov.cinemarestservice.exception.AlreadyPurchasedTicketException;
 import com.dteliukov.cinemarestservice.exception.NotFoundTicketException;
 import com.dteliukov.cinemarestservice.exception.SeatOutOfBoundsException;
+import com.dteliukov.cinemarestservice.exception.WrongPasswordException;
 import com.dteliukov.cinemarestservice.model.common.PurchasedTicket;
 import com.dteliukov.cinemarestservice.model.common.Seat;
 import com.dteliukov.cinemarestservice.model.common.Ticket;
 import com.dteliukov.cinemarestservice.model.request.Token;
 import com.dteliukov.cinemarestservice.model.response.RoomInfo;
+import com.dteliukov.cinemarestservice.model.response.Stats;
 import com.dteliukov.cinemarestservice.service.RoomService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -39,6 +41,16 @@ public class CinemaController {
     @PostMapping("/return")
     public Ticket returnTicket(@RequestBody Token token) {
         return roomService.returnTicket(token);
+    }
+
+    @PostMapping("/stats")
+    public Stats getStats(@RequestParam(required = false) String password) {
+        return roomService.getRoomStats(password);
+    }
+
+    @ExceptionHandler(WrongPasswordException.class)
+    public ResponseEntity<?> handlerWrongPassword(WrongPasswordException e) {
+        return new ResponseEntity<>(errorMessage(e.getMessage()), HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(NotFoundTicketException.class)
