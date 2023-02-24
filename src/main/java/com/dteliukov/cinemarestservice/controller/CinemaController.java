a179796b4d2d09265f907f9210a8a755556f684d
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-02-24 14:47:19 +0200

    Add endpoint "/purchase" in controller CinemaController

--- src/main/java/com/dteliukov/cinemarestservice/controller/CinemaController.java
+++ src/main/java/com/dteliukov/cinemarestservice/controller/CinemaController.java
@@ -1,11 +1,21 @@
 package com.dteliukov.cinemarestservice.controller;
 
+import com.dteliukov.cinemarestservice.exception.AlreadyPurchasedTicketException;
+import com.dteliukov.cinemarestservice.exception.SeatOutOfBoundsException;
+import com.dteliukov.cinemarestservice.model.common.PurchasedTicket;
+import com.dteliukov.cinemarestservice.model.common.Seat;
 import com.dteliukov.cinemarestservice.model.response.RoomInfo;
 import com.dteliukov.cinemarestservice.service.RoomService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,5 +26,26 @@ public class CinemaController {
     @GetMapping("/seats")
     public RoomInfo getRoom() {
         return roomService.getRoomInfo();
+    }
+
+    @PostMapping("/purchase")
+    public PurchasedTicket purchase(@RequestBody Seat seat) {
+        return roomService.purchase(seat);
+    }
+
+    @ExceptionHandler(SeatOutOfBoundsException.class)
+    public ResponseEntity<?> handlerSeatOutOfBoundsException(SeatOutOfBoundsException e) {
+        return new ResponseEntity<>(errorMessage(e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AlreadyPurchasedTicketException.class)
+    public ResponseEntity<?> handlerAlreadyPurchasedTicket(AlreadyPurchasedTicketException e) {
+        return new ResponseEntity<>(errorMessage(e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    private Map<String, String> errorMessage(String message) {
+        Map<String, String> body = new HashMap<>();
+        body.put("error", message);
+        return body;
     }
 }
